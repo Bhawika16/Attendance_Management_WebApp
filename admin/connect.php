f47f4ab0b90f367a7bc1c2fd86b3f2f9605d1f3d
Bhawika16 <62383299+Bhawika16@users.noreply.github.com>
2021-05-18 18:38:21 +0530

    Add files via upload

---
+++ admin/connect.php
@@ -0,0 +1,6 @@
+<?php
+//establishing connection with database.
+
+$con=mysqli_connect('localhost','root','') or die('Cannot connect to server');
+mysqli_select_db($con,'attdata') or die ('Cannot found database');
+?>
